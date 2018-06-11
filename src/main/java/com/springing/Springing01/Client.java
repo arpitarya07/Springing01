90d3ac624b516c114dc7a4db2dba676932a1ab56
Arpit Arya <arpitarya07@gmail.com>
2018-06-10 20:04:20 -0400

    Setter injection using application.xml

--- src/main/java/com/springing/Springing01/Client.java
+++ src/main/java/com/springing/Springing01/Client.java
@@ -9,17 +9,22 @@ import com.springing.Springing01.service.MessagePrinterService;
  * Hello world!
  *
  */
-public class Client 
-{
+public class Client {
 	public static void main(final String[] args) {
 
-		//configuration part	
-		final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/application.xml");
-		final MessagePrinterService printer = (MessagePrinterService) context.getBean("printer");		
-		
-		//doing something part...
-		printer.printMessage();
-		
+		// configuration part
+		try (final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring/application.xml")) {
+			context.registerShutdownHook();
+			final MessagePrinterService printer = (MessagePrinterService) context.getBean("printer");
+
+			// doing something part...
+			printer.printMessage();
+		}
+
+		catch (final Exception ex) {
+			ex.printStackTrace();
+		}
 
 	}
 }
--- src/main/java/com/springing/Springing01/service/BasicMessageOfTheDayImpl.java
+++ src/main/java/com/springing/Springing01/service/BasicMessageOfTheDayImpl.java
@@ -2,7 +2,13 @@ package com.springing.Springing01.service;
 
 public class BasicMessageOfTheDayImpl implements MessageOfTheDayService {
 	
+	private String message;
+	
 		public String getMessage() {
-			return "Hello World";
+			return message;
+		}
+		
+		public void setMessage(String message) {
+			this.message = message;
 		}
 }
