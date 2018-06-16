cccdf3f4519ab611b029fcc3672342c3b35901c2
Arpit Arya <arpitarya07@gmail.com>
2018-06-16 17:05:13 -0400

    bean Scopes

--- src/main/java/com/springing/Springing01/Client.java
+++ src/main/java/com/springing/Springing01/Client.java
@@ -2,6 +2,7 @@ package com.springing.Springing01;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.springing.Springing01.service.BasicMessageOfTheDayImpl;
 import com.springing.Springing01.service.DynamicMessageOfTheDayImpl;
 import com.springing.Springing01.service.MessagePrinterService;
 
@@ -16,10 +17,18 @@ public class Client {
 		try (final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"spring/application.xml")) {
 			context.registerShutdownHook();
-			final MessagePrinterService printer = (MessagePrinterService) context.getBean("printer");
+			
+			final BasicMessageOfTheDayImpl basicMessageService = (BasicMessageOfTheDayImpl) context.getBean("basicMessageOfTheDay");
+			basicMessageService.setMessage("Hiiii");
+			System.out.println(basicMessageService.getMessage());
+			
+			final BasicMessageOfTheDayImpl basicMessageService2 = (BasicMessageOfTheDayImpl) context.getBean("basicMessageOfTheDay");
+			// basicMessageService.setMessage("Hiiii");
+			System.out.println(basicMessageService2.getMessage());
+			// final MessagePrinterService printer = (MessagePrinterService) context.getBean("printer");
 
 			// doing something part...
-			printer.printMessage();
+			// printer.printMessage();
 		}
 
 		catch (final Exception ex) {
